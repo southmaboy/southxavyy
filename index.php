d8981ea6c36034284d2e5dfb49898c3462618d1c
southmaboy <166618605+southmaboy@users.noreply.github.com>
2025-03-07 14:18:31 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-if (preg_match('/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i', $userAgent)) {
-    header("Location: https://m.southxavyy.pl" . $_SERVER['REQUEST_URI']);
-    exit();
-}
-?>
